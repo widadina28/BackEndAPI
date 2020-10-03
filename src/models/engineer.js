2fd7ce47642cdbd5c128698e45a5d050d58f5964
widadina28 <rosyida.ulya@gmail.com>
2020-10-03 20:59:49 +0700

    push

--- src/models/engineer.js
+++ src/models/engineer.js
@@ -50,7 +50,7 @@ module.exports = {
   },
   getDataEngineerModel: (orderKey, searchKey, searchValue, limit, offset) => {
     return new Promise((resolve, reject) => {
-      db.query(`SELECT engineer.id_engineer, engineer.name_engineer, GROUP_CONCAT(skill.name_skill) AS name_skill, freelance.name_freelance, account.name_account, location.name_loc, engineer.cost, engineer.rate, engineer.description_engineer, engineer.image, engineer.createAt, engineer.updateAt, engineer.status, (SELECT COUNT(*) FROM engineer) as count FROM engineer JOIN expertise ON engineer.id_engineer=expertise.id_engineer JOIN skill ON skill.id_skill=expertise.id_skill JOIN freelance ON freelance.id_freelance=engineer.id_freelance JOIN account ON account.id_account=engineer.id_account JOIN location ON location.id_loc=engineer.id_loc WHERE ${searchKey} LIKE '%${searchValue}%' GROUP BY expertise.id_engineer ORDER BY ${orderKey} DESC LIMIT ${limit}  OFFSET ${offset}`, (err, result, _fields) => {
+      db.query(`SELECT engineer.id_engineer, engineer.name_engineer, GROUP_CONCAT(skill.name_skill) AS name_skill, freelance.name_freelance, location.name_loc, engineer.cost, engineer.rate, engineer.image, engineer.status, (SELECT COUNT(*) FROM engineer) as count FROM engineer JOIN expertise ON engineer.id_engineer=expertise.id_engineer JOIN skill ON skill.id_skill=expertise.id_skill JOIN freelance ON freelance.id_freelance=engineer.id_freelance JOIN location ON location.id_loc=engineer.id_loc WHERE ${searchKey} LIKE '%${searchValue}%' GROUP BY expertise.id_engineer ORDER BY ${orderKey} DESC LIMIT ${limit}  OFFSET ${offset}`, (err, result, _fields) => {
         if (err) {
           reject(new Error(err))
         } else {
